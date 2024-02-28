085c4f856e26a4ccf48274dee1dd03e92f3dd51f
Lê Thanh Phát <ltphatt2709@gmail.com>
2024-02-28 08:45:20 +0700

    Add music and sound effects

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [Header("Click Sound")]
+    [SerializeField] AudioClip clickSound;
+    [SerializeField][Range(0f, 1f)] float clickSoundVolume = 1f;
+
+    public static AudioPlayer instance;
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void PLayClickSound()
+    {
+        PlayClip(clickSound, clickSoundVolume);
+    }
+
+    void PlayClip(AudioClip clip, float volume)
+    {
+        if (clip != null)
+        {
+            Vector3 cameraPosition = Camera.main.transform.position;
+            AudioSource.PlayClipAtPoint(clip, cameraPosition, volume);
+        }
+    }
+}
